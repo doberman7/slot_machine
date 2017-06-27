619348ac13ae26b9ce713af63db7942439e6b3a1
codeamac19 <codeamac19@gmail.com>
2017-06-27 17:46:10 -0500

    funcional

--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
-  console.log("pre Ajax");
+  //console.log("pre Ajax");
   $('#formulario').on('submit',function() {
-    //console.log(" function on click");
+    //inperdir que la solicitud no xhr suceda
     event.preventDefault();
-
-
-    $.ajax({
-      url: '/rolls',
-      method: 'POST',
-      // AQUI
-      success: $('div').html('#die')
+    //Load data from the server using a HTTP POST request.
+    $.post('/rolls',function(data){
+      //en la vista index apuntar al id=die y traer el callback
+      /*DATA es algo asì, los numeros cambian aleatoriamente:
+                <img src="/2.png" title="2" alt="the roll" id="image-1">
+                <img src="/1.png" title="1" alt="the roll" id="image-2">
+                <img src="/3.png" title="3" alt="the roll" id="image-3">*/
+      console.log(data);
+      $('#die').html(data);
     });
   });
-  console.log("post Ajax");
+  //console.log("post Ajax");
 });
