b28bdd8de8dcb72a5f54b57c1b06b5c24bbc7770
codeamac19 <codeamac19@gmail.com>
2017-06-26 21:27:05 -0500

    desarrollando AJAX request2

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,17 +1,17 @@
 get '/' do
   @rolls = []
-
+  p "/" * 50
   3.times { @rolls << Roll.create }
-
-  @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
-
+  #Mostrar "WINER" si al mapear Rolls, solo hay un unico value
+  p @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
+  p "/" * 50
   erb :index
 end
 
 # QUE HACER?: Convierte esta ruta para que utilice Ajax y si la petición no es
 # de Ajax de igual forma sirva.
 post '/rolls' do
-
+  p "+"*50
   @rolls = []
 
   if params[:value]
@@ -19,8 +19,17 @@ post '/rolls' do
   else
     3.times { @rolls << Roll.create }
   end
-
+  # Se dejan solo los valores de cada Roll en el Ary, se eliminan los repetidos, y si al la cuenta de elementos dentro del Ary es 1, @win es "WINER"
   @win = "WINNER!!" if @rolls.map! { |roll| roll.value }.uniq.count == 1
+  p "+"*50
+  # erb :index  # TIP: Qué esté haciendo esto y qué debe hacer diferente.
+  if request.xhr?
+     p "request.xhr?" + "-"*50
+    # erb :index
+    erb :_die_roll, layout: false 
+    else
+      p "no xhr " + "-"*50
+      erb :index
+  end
 
-  erb :index  # TIP: Qué esté haciendo esto y qué debe hacer diferente.
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
+  console.log("pre Ajax");
+  $('#formulario').on('submit',function() {
+    //console.log(" function on click");
+    event.preventDefault();
+
+
+    $.ajax({
+      url: '/rolls',
+      method: 'POST',
+      // AQUI
+      success: $('div').html('#die')
+    });
+  });
+  console.log("post Ajax");
 });
